0675717de03222ac91863f7ff19aca6c30bbd64e
Gustavo <gdp2116@columbia.edu>
2019-03-14 21:26:21 -0400

    Local `reveal.js`; theme becomes `solarized`; change pandoc params

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 # Builds the presentation on Contents.md
 pandoc -t revealjs -s -o Contents.html Contents.md \
-       -V revealjs-url=http://lab.hakim.se/reveal-js/js/reveal.js\
-       -V theme=serif \
+       -V revealjs-url=/home/gustavo/.local/reveal.js \
+       -V theme=solarized\
        -V transition=none\
        -V 'center:false'\
-       --include-in-header custom.html\
+       -V 'margin: 0.1'\
+       -V 'width: 1024'\
+       -V 'height: 768'\
+       --slide-level=2\
+       --include-in-header custom.html
 
 
 
@@ -14,7 +18,12 @@ pandoc -t revealjs -s -o Contents.html Contents.md \
 # --mathjax
 # -V 'width:"80%"'
 # -V 'height:"80%"'
+# -V 'width:"80%"'
+# -V 'height:"80%"'
 # --include-in-header custom.html
 # --self-contained
 # http://lab.hakim.se/reveal-js/js/reveal.js
 # -V revealjs-url=/home/gustavo/.local/reveal.js \
+# -V 'center:false'\
+# -V 'width: 1024'\
+# -V 'height: 768'\
